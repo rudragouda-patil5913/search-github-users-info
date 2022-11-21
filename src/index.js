1665df81726c4bf628598dc6268ec1402dc524df
unknown <rudragouda.spatil1298@gmail.com>
2022-11-21 21:45:26 +0530

    initial commit of github users search

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+//dev-eyy8cosz.us.auth0.com
+//Cx78G8Z8hWsh3NRrta3djc90Il4jXg6V
+ReactDOM.render(
+  <React.StrictMode>
+  
+   <Auth0Provider
+      domain='dev-eyy8cosz.us.auth0.com'
+      clientId='Cx78G8Z8hWsh3NRrta3djc90Il4jXg6V'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+  
+  <GithubProvider>
+    <App />
+  </GithubProvider>
+  </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
